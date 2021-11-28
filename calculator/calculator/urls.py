d6340e0a2c599a4e9fe6251d57700f026e508c8b
LNagad <nagadgames100k@gmail.com>
2021-11-28 16:46:37 -0400

    datos llegan a python

--- calculator/calculator/urls.py
+++ calculator/calculator/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from calculator.vista import index
+from calculator.vista import buscar,index
+
+
+# estructura: from proyecto.vista import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index)
+    path('index/', index),
+    path('buscar/', buscar)
 ]
--- calculator/calculator/vista.py
+++ calculator/calculator/vista.py
@@ -1,5 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 def index(request):
     return render (request, 'index.html')
+
+def buscar(request):
+    mensaje= "llego el calculo %r" %request.GET["input_solved"]
+
+    return HttpResponse(mensaje)
