4e88f7707cf7c1abab79f5cca0d47ae298e38f30
LNagad <nagadgames100k@gmail.com>
2021-11-28 21:23:36 -0400

    lectura y escritura de json con js y django creada

---
+++ calculator/calculator/web/static/js/json.js
@@ -0,0 +1,35 @@
+// ----------- reading the json
+function readJson() {
+    const data = require('./data.json')
+
+    let resultado = data.resultado
+    console.log(resultado)
+    
+    // return resultado
+}
+
+
+// -------------- writing and creating a json file
+function writeJson(operacion) {
+    const fs = require('fs')
+
+    const calculateJson = {
+        calculo: '150+150',
+        resultado: 300
+    }
+    // ---------------- convert dict to json
+    
+    // const jsonString = JSON.stringify(newObject);
+    // console.log(jsonString)
+
+    fs.writeFile('./data2.json', JSON.stringify(calculateJson, null, 2), err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('File successfully written')
+        }
+    })
+}
+
+
+writeJson()
---
+++ calculator/calculator/web/static/py/prueba.py
@@ -0,0 +1,66 @@
+import json
+
+#Descifrar un json
+
+'''
+json_str = '{"nombre":"Maycol","edad":18,"pais":"rd","telefono":809456878}'
+
+
+print(json_str)
+print(type(json_str))
+
+python_dict = json.loads(json_str)
+
+print(python_dict['nombre'])
+
+'''
+
+#---------------- Crear un json
+
+data = {
+    "nombre": "Maycol",
+    "edad": 19,
+    "pais": "republica dom",
+    "telefono": 809458555,
+    "cursos": ["PHP", "Python", "JavaScript", "C#", "Node.js"]
+}
+
+json_data = json.dumps(data)
+# print(json_data)
+# print(type(json_data))
+
+# ------------ subdividir el json o fragmentarlo o cortarlo por pedasitos
+
+"""
+mis_datos = json.loads(json_data)
+
+datos_php = mis_datos['cursos'][0]
+datos_python = mis_datos['cursos'][1]
+datos_js = mis_datos['cursos'][2]
+datos_csharp = mis_datos['cursos'][3]
+datos_node = mis_datos['cursos'][4]
+
+print(f'''Cursos:
+1. {datos_node} 
+2. {datos_python}
+3. {datos_js}''') 
+
+"""
+
+
+#--------------- nuevos metodos crear json
+'''
+json_data = json.JSONEncoder().encode({"resultado":500, "status":"ok"})
+print(json_data)
+print(type(json_data))
+print('\n')
+'''
+#--------------- nuevos metodos crear diccionario o dict
+'''
+python_dict = json.JSONDecoder().decode(json_data)
+print(python_dict)
+print(type(python_dict))
+'''
+
+
+
